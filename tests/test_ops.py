6e5a1572c9663d5c2225f74dea8ac1f853d5af41
Martin Hwang <dhhwang89@gmail.com>
2020-07-25 16:34:02 +0900

    feat: pytorch seed 고정 예시 추가
     Refs: #19

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,18 +1,33 @@
+import torch
 import pytest
+
+import pytorch_lightning
 
 from src.model.ops import add, multiply, subtract
 
 
 def test_multiply():
+    pytorch_lightning.seed_everything(777)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     result = multiply(2, 3)
     assert result == 6
 
 
 def test_add():
+    pytorch_lightning.seed_everything(777)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     result = add(2, 3)
     assert result == 5
 
 
 def test_subtract():
+    pytorch_lightning.seed_everything(777)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
     result = subtract(3, 2)
     assert result == 1
--- train.py
+++ train.py
@@ -2,3 +2,9 @@
     This script was made by Nick at 19/07/20.
     To implement code for training your model.
 """
+import torch
+import pytorch_lightning
+
+pytorch_lightning.seed_everything(777)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
